2cab32df88ded7730248e4e103d78c07775bf22b
ccellis <colemancellis@gmail.com>
2023-11-19 14:46:02 -0500

    Start at chapter 5 of Zero to Production

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use sqlx::postgres::PgPoolOptions;
+use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
+}
